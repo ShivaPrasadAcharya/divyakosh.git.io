d6f4beae498d69023c9958e3c0ba68d82d1537a3
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2024-12-26 16:39:13 +0545

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/divyakosh.git.io/',
+  build: {
+    outDir: 'docs', // Change build output to 'docs' folder
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
