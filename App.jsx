0f3074cbb33c0e0e724b31d075ff7f549d64830e
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2024-12-26 15:20:53 +0545

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SpiritualDictionary from './components/SpiritualDictionary'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <SpiritualDictionary />
+    </div>
+  )
+}
+
+export default App
